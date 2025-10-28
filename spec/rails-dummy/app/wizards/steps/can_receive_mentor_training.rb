e70a4c0189e9f71a7dc65d7ba9a5f80d44a9db06
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2025-10-28 20:41:50 +0000

    Add link (to go down LP path)

--- spec/rails-dummy/app/wizards/steps/can_receive_mentor_training.rb
+++ spec/rails-dummy/app/wizards/steps/can_receive_mentor_training.rb
@@ -1,9 +1,10 @@
 module Steps
   class CanReceiveMentorTraining < DfE::Wizard::Step
     attribute :mentor_id, :integer
+    attribute :lp_will_provide, :string
 
     def self.permitted_params
-      %w[mentor_id]
+      %w[mentor_id lp_will_provide]
     end
 
     def mentor
