c5ef7f0a0ac8d103ad65b6fef6e4cc4f6da2e6f8
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2025-12-10 18:08:08 +0000

    Add review ect details step

--- spec/rails-dummy/app/wizards/steps/register_ect/review_ect_details_step.rb
+++ spec/rails-dummy/app/wizards/steps/register_ect/review_ect_details_step.rb
@@ -2,6 +2,16 @@ module Steps
   module RegisterECT
     class ReviewECTDetailsStep
       include DfE::Wizard::Step
+
+      attribute :details_correct, :string
+      attribute :correct_full_name, :string
+
+      validates :details_correct, presence: true
+      validates :correct_full_name, presence: true, if: -> { details_correct == 'no' }
+
+      def self.permitted_params
+        %w[details_correct correct_full_name]
+      end
     end
   end
 end
