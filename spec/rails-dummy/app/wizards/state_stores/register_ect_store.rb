c7bd41b22c5415e8ba36f0ec6ce93cfc3e1c35e4
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2025-12-10 16:56:03 +0000

    Add logic for redirects

--- spec/rails-dummy/app/wizards/state_stores/register_ect_store.rb
+++ spec/rails-dummy/app/wizards/state_stores/register_ect_store.rb
@@ -3,23 +3,39 @@ module StateStores
     include DfE::Wizard::StateStore
 
     def in_trs?
-      data[:trn][:attribute] == :foo
+      trn != '0000000'
     end
 
-    def matches_trs_dob?; end
+    def matches_trs_dob?
+      trn != '1111111'
+    end
 
-    def active_at_school?; end
+    def active_at_school?
+      trn == '2222222'
+    end
 
-    def induction_exempt?; end
+    def induction_completed?
+      trn == '3333333'
+    end
 
-    def prohibited_from_teaching?; end
+    def induction_exempt?
+      trn == '4444444'
+    end
 
-    def induction_completed?; end
+    def prohibited_from_teaching?
+      trn == '5555555'
+    end
 
     def cant_use_email?; end
 
     def school_independent?; end
 
     def provider_led?; end
+
+    private
+
+    def trn
+      read[:trn].to_s
+    end
   end
 end
