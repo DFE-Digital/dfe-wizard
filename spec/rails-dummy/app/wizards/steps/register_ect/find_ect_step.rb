99e24f9663b7992890adfd05aae16f5c38371a44
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2025-12-10 16:55:40 +0000

    Add FindECTStep

--- spec/rails-dummy/app/wizards/steps/register_ect/find_ect_step.rb
+++ spec/rails-dummy/app/wizards/steps/register_ect/find_ect_step.rb
@@ -2,6 +2,14 @@ module Steps
   module RegisterECT
     class FindECTStep
       include DfE::Wizard::Step
+
+      attribute :trn, :string
+
+      validates :trn, presence: true
+
+      def self.permitted_params
+        %w[trn]
+      end
     end
   end
 end
