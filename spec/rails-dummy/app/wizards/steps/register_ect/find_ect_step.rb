4f40705e02062a3b2ed98c93b603ea5c3f97c167
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2025-12-10 18:02:29 +0000

    Persist dates across pages

--- spec/rails-dummy/app/wizards/steps/register_ect/find_ect_step.rb
+++ spec/rails-dummy/app/wizards/steps/register_ect/find_ect_step.rb
@@ -4,11 +4,29 @@ module Steps
       include DfE::Wizard::Step
 
       attribute :trn, :string
+      attribute :date_of_birth, :date
 
       validates :trn, presence: true
+      validates :date_of_birth, presence: true
 
       def self.permitted_params
-        %w[trn]
+        %w[trn date_of_birth(1i) date_of_birth(2i) date_of_birth(3i)]
+      end
+
+      def assign_attributes(attrs = {})
+        date_parts = attrs.symbolize_keys!.extract!(:'date_of_birth(1i)', :'date_of_birth(2i)', :'date_of_birth(3i)')
+
+        super
+
+        if date_parts.values.all?(&:present?)
+          self.date_of_birth = Date.new(
+            date_parts[:'date_of_birth(1i)'].to_i,
+            date_parts[:'date_of_birth(2i)'].to_i,
+            date_parts[:'date_of_birth(3i)'].to_i,
+          )
+        end
+      rescue ArgumentError
+        self.date_of_birth = nil
       end
     end
   end
