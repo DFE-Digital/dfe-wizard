cffa277d74b9678b0be61aecc95a7f81c3c04d31
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2025-12-10 18:03:36 +0000

    Add email step

--- spec/rails-dummy/app/wizards/steps/register_ect/email_address_step.rb
+++ spec/rails-dummy/app/wizards/steps/register_ect/email_address_step.rb
@@ -2,6 +2,14 @@ module Steps
   module RegisterECT
     class EmailAddressStep
       include DfE::Wizard::Step
+
+      attribute :email, :string
+
+      validates :email, presence: true
+
+      def self.permitted_params
+        %w[email]
+      end
     end
   end
 end
