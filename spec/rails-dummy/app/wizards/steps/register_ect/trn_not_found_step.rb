207b096d5522e4cf4c372404db9a990c7ffd4371
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2025-12-10 18:24:43 +0000

    Add national insurance step

--- spec/rails-dummy/app/wizards/steps/register_ect/trn_not_found_step.rb
+++ spec/rails-dummy/app/wizards/steps/register_ect/trn_not_found_step.rb
@@ -2,6 +2,14 @@ module Steps
   module RegisterECT
     class TRNNotFoundStep
       include DfE::Wizard::Step
+
+      attribute :national_insurance_number, :string
+
+      validates :national_insurance_number, presence: true
+
+      def self.permitted_params
+        %w[national_insurance_number]
+      end
     end
   end
 end
