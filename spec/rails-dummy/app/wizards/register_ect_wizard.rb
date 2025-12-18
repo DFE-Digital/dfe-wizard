186eaf5df14c12ba7f223d53fd82d369703d42a5
Tomas D'Stefano <tomas_stefano@successoft.com>
2025-12-18 18:21:26 +0000

    Fix register ect return to review bugs on dummy app

--- spec/rails-dummy/app/wizards/register_ect_wizard.rb
+++ spec/rails-dummy/app/wizards/register_ect_wizard.rb
@@ -1,15 +1,6 @@
-# 1.0.0 future
-#
-# RegisterECTWizard.new(
-# current_step: :find_ect,
-# state_store: SomeStore.new(),
-# step_params: {}
-# )
-#
 class RegisterECTWizard
   include DfE::Wizard
 
-  include Steps
   delegate :in_trs?,
            :matches_trs_dob?,
            :active_at_school?,
@@ -50,7 +41,6 @@ class RegisterECTWizard
       graph.add_custom_branching_edge(
         from: :find_ect,
         conditional: :find_ect_transitions,
-        # this option is only for auto-generated documentation
         potential_transitions: [
           { label: 'TRN not found', nodes: [:trn_not_found] },
           { label: 'National Insurance number', nodes: [:national_insurance_number] },
@@ -62,32 +52,9 @@ class RegisterECTWizard
         ],
       )
 
-      # This method does not exist yet.
-      # graph.add_conditional_edges(
-      #  from: :national_insurance_number,
-      #  transitions: [
-      #    {
-      #      when: :in_trs?,
-      #      then: :not_found,
-      #    },
-      #    {
-      #      when: :induction_completed?,
-      #      then: :induction_completed
-      #    },
-      #    {
-      #      when: :induction_exempt?,
-      #      then: :induction_exempt
-      #    }
-      #  ],
-      #  else: :review_ect_details,
-      #  label: 'Review ECT details',
-      # )
-
-      # Or the above you can do as below:
       graph.add_custom_branching_edge(
         from: :national_insurance_number,
         conditional: :national_insurance_number_transitions,
-        # this option is only for auto-generated documentation
         potential_transitions: [
           { label: 'In TRS?', nodes: [:not_found] },
           { label: 'Induction completed?', nodes: [:induction_completed] },
@@ -103,7 +70,6 @@ class RegisterECTWizard
         when: :cant_use_email?,
         then: :cant_use_email,
         else: :start_date,
-        label: 'Can not use email',
       )
 
       graph.add_edge from: :start_date, to: :working_pattern
@@ -113,7 +79,6 @@ class RegisterECTWizard
         when: :school_independent?,
         then: :independent_school_appropriate_body,
         else: :state_school_appropriate_body,
-        label: 'School independent?',
       )
 
       graph.add_edge from: :independent_school_appropriate_body, to: :programme_type
@@ -124,7 +89,6 @@ class RegisterECTWizard
         when: :provider_led?,
         then: :lead_provider,
         else: :check_answers,
-        label: 'School independent?',
       )
 
       graph.add_edge from: :lead_provider, to: :check_answers
@@ -146,63 +110,16 @@ class RegisterECTWizard
     DfE::Wizard::Logger.new(Rails.logger) if Rails.env.local?
   end
 
-  # Given you have a wizard like these:
-  #
-  # A -> B -> C -> D -> CYA
-  #      |              |
-  #      -> E  -> F ->  G
-  #
-  # If you were on A -> B -> C -> D -> CYA and the click change on "B" user
-  # returned to B step.
-  #
-  # Then on B you answer something else that needs to go to E.
-  #
-  # then you don't have a full traversal to CYA anymore and need now to
-  # go to E
-  #
-  #  path_traversal(A -> CYA) => [] # will return empty
-  #
-  # Then answer E, still not up to CYA then go to G
-  #
-  #  path_traversal(A -> CYA) => [] # will return empty
-  #
-  # Then G user answer end up on CYA then is a full path traversal return CYA.
-  #
-  #     path_traversal(A -> CYA) => [:A, :B, :E, :F, :G, :CYA]
-  #
   def next_step_override
-    target = current_step_params[:return_to_review]
+    target = @current_step_params[:return_to_review]
 
-    target if user_up_to_check_answers? && target.present?
+    :check_answers if target.present? && valid_path_to?(:check_answers)
   end
 
-  # return_to_review="B" B is the page that the user click "Change"
-  #
-  # A -> B -> C -> D -> CYA
-  #      |              |
-  #      -> E  -> F ->  G
-  #
-  # If you were on A -> B -> C -> D -> CYA and the click change on "B"
-  # the previous step will go to CYA
-  #
-  #  ?return_to_review=B
-  #
-  #  the user change branch and to E
-  #
-  #  ?return_to_review=B so previous step will be B
-  #
   def previous_step_override
-    target = current_step_params[:return_to_review]&.to_sym
+    target = @current_step_params[:return_to_review]&.to_sym
 
-    target if user_up_to_check_answers? && full_traversal.include?(target)
-  end
-
-  def user_up_to_check_answers?
-    full_traversal.include?(:check_answers)
-  end
-
-  def full_traversal
-    steps_processor.path_traversal(:check_answers)
+    :check_answers if current_step_name == target && valid_path_to?(:check_answers)
   end
 
   def find_ect_transitions
@@ -217,7 +134,7 @@ class RegisterECTWizard
     :review_ect_details
   end
 
-  def national_insurance_number_transitions(_data)
+  def national_insurance_number_transitions
     return :not_found unless in_trs?
     return :induction_completed if induction_completed?
     return :induction_exempt if induction_exempt?
@@ -225,66 +142,3 @@ class RegisterECTWizard
     :review_ect_details
   end
 end
-
-# 0.1.1
-# module Schools
-#  module RegisterECTWizard
-#    class Wizard < DfE::Wizard::Base
-#      attr_accessor :store, :school
-#
-#      steps do
-#        [
-#          {
-#            already_active_at_school: AlreadyActiveAtSchoolStep,
-#            cannot_register_ect: CannotRegisterECTStep,
-#            cant_use_email: CantUseEmailStep,
-#            change_email_address: ChangeEmailAddressStep,
-#            change_independent_school_appropriate_body: ChangeIndependentSchoolAppropriateBodyStep,
-#            change_lead_provider: ChangeLeadProviderStep,
-#            change_programme_type: ChangeProgrammeTypeStep,
-#            change_review_ect_details: ChangeReviewECTDetailsStep,
-#            change_start_date: ChangeStartDateStep,
-#            change_state_school_appropriate_body: ChangeStateSchoolAppropriateBodyStep,
-#            change_working_pattern: ChangeWorkingPatternStep,
-#            check_answers: CheckAnswersStep,
-#            confirmation: ConfirmationStep,
-#            email_address: EmailAddressStep,
-#            find_ect: FindECTStep,
-#            independent_school_appropriate_body: IndependentSchoolAppropriateBodyStep,
-#            induction_completed: InductionCompletedStep,
-#            induction_exempt: InductionExemptStep,
-#            lead_provider: LeadProviderStep,
-#            national_insurance_number: NationalInsuranceNumberStep,
-#            not_found: NotFoundStep,
-#            programme_type: ProgrammeTypeStep,
-#            review_ect_details: ReviewECTDetailsStep,
-#            start_date: StartDateStep,
-#            state_school_appropriate_body: StateSchoolAppropriateBodyStep,
-#            trn_not_found: TRNNotFoundStep,
-#            working_pattern: WorkingPatternStep,
-#          }
-#        ]
-#      end
-#
-#      def self.step?(step_name)
-#        Array(steps).first[step_name].present?
-#      end
-#
-#      delegate :save!, to: :current_step
-#      delegate :reset, to: :ect
-#
-#      def ect
-#        @ect ||= ECT.new(store)
-#      end
-#
-#      def appropriate_bodies
-#        @appropriate_bodies ||= AppropriateBody.select(:id, :name).all
-#      end
-#
-#      # OPTIMIZE: May eventually depend on the ECT being registered and move to Schools::RegisterECTWizard::ECT
-#      def lead_providers
-#        @lead_providers ||= LeadProvider.select(:id, :name).all
-#      end
-#    end
-#  end
-# end
--- spec/rails-dummy/app/wizards/steps/register_ect/state_school_appropriate_body_step.rb
+++ spec/rails-dummy/app/wizards/steps/register_ect/state_school_appropriate_body_step.rb
@@ -2,6 +2,12 @@ module Steps
   module RegisterECT
     class StateSchoolAppropriateBodyStep
       include DfE::Wizard::Step
+
+      attribute :appropriate_body_name, :string
+      validates :appropriate_body_name,
+                presence: {
+                  message: "Enter the name of the appropriate body which will be supporting the ECT's induction",
+                }
     end
   end
 end
